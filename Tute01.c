f66ad143f2f5772fdf28b6683bba210d6413a23a
IT21040726 <87372832+it21040726@users.noreply.github.com>
2021-07-13 13:31:46 +0000

    I have completed all question

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,19 @@
 #include <stdio.h>
 
 int main() {
-  
+int mark1, mark2;
+ float avg;
+
+ printf("Enter mark the subject 1= ");
+ scanf("%d", &mark1);
+
+  printf("Enter mark the subject 2= ");
+ scanf("%d", &mark2);
+ 
+ avg = (float)(mark1 + mark2) /2;
+
+ printf("Average of %d and %d is: %.2f",mark1,mark1,avg);
+
   return 0;
 }
 
--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,21 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
+int distance,amount;
+  printf("Enter the distance=");
+  scanf("%d",&distance);
+
+  if(distance < 30)
+  {
+    amount = distance *50;  
+  }
+  else 
+  {
+    amount = 30 * 50 +((distance - 30)*40); 
+  }
+  printf("amount is=%d",amount);
+
+
   
   return 0;
 }
